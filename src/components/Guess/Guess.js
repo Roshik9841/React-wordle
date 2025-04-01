829cbd1aad4f50c1cfedc201fb0e1a868790751c
I am Roshik <Roshik9841@gmail.com>
2025-04-01 15:57:14 +0545

    React_wordle

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Guess({value}) {
+  return(
+   
+   
+    <p class="guess">
+      {value.split('').map((letter,index)=>(
+          <span className="cell" key={index}>{letter}</span>
+      ))}
+    
+
+  </p>
+  );
+}
+
+export default Guess;
